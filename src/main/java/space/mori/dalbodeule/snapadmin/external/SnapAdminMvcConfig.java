61ff240456541fb20174cce6c2b90e76e8aa9307
dalbodeule <11470513+dalbodeule@users.noreply.github.com>
2025-05-22 00:50:02 +0900

    Remove @EnableWebMvc annotation from SnapAdminMvcConfig
    The `@EnableWebMvc` annotation was removed to prevent overriding Spring Boot's default MVC configuration. This change ensures better compatibility with existing configurations and avoids potential conflicts in the application.

--- src/main/java/space/mori/dalbodeule/snapadmin/external/SnapAdminMvcConfig.java
+++ src/main/java/space/mori/dalbodeule/snapadmin/external/SnapAdminMvcConfig.java
@@ -22,12 +22,10 @@ package space.mori.dalbodeule.snapadmin.external;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class SnapAdminMvcConfig implements WebMvcConfigurer {
 	@Autowired
 	private SnapAdminProperties properties;
